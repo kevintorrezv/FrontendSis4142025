d8531c06ab2310a74a4cdb02dc4e6c958c7b7cf4
DanielaAlejandraMamaniSilva <danielaalejandramamanisilva@gmail.com>
2025-12-01 18:50:50 -0400

    url ngrok

--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -34,40 +34,40 @@ export class ProductoService {
 
   // 1. Endpoint para Escobas: http://localhost:8080/api/escobas
   obtenerTodasLasEscobas(): Observable<Escoba[]> {
-    return this.http.get<Escoba[]>(`https://semihumanized-stoopingly-emerald.ngrok-free.dev/api/escobas`); 
+    return this.http.get<Escoba[]>(`${API_URL}/escobas`); 
   }
 
   // 2. Endpoint para Fabricantes: http://localhost:8080/api/fabricantes
   obtenerTodosLosFabricantes(): Observable<Fabricante[]> {
-    return this.http.get<Fabricante[]>(`https://semihumanized-stoopingly-emerald.ngrok-free.dev/api/fabricantes`); 
+    return this.http.get<Fabricante[]>(`${API_URL}/fabricantes`); 
   }
 
     // Endpoint para ELIMINAR una Escoba (DELETE /api/escobas/{id})
   eliminarEscoba(id: number): Observable<any> {
     // Nota: El backend de Java suele esperar un ID en la URL para DELETE
-    return this.http.delete(`https://semihumanized-stoopingly-emerald.ngrok-free.dev/api/escobas/${id}`); 
+    return this.http.delete(`${API_URL}/escobas/${id}`); 
   }
 
   // Endpoint para ELIMINAR un Fabricante (DELETE /api/fabricantes/{id})
   eliminarFabricante(id: number): Observable<any> {
-    return this.http.delete(`https://semihumanized-stoopingly-emerald.ngrok-free.dev/api/fabricantes/${id}`);
+    return this.http.delete(`${API_URL}/fabricantes/${id}`);
   }
    crearFabricante(fabricante: Fabricante): Observable<Fabricante> {
     // Nota: enviamos el objeto Fabricante sin el campo 'escobas'
     // El Backend de Java debe recibir un objeto Fabricante
-    return this.http.post<Fabricante>(`https://semihumanized-stoopingly-emerald.ngrok-free.dev/api/fabricantes`, fabricante);
+    return this.http.post<Fabricante>(`${API_URL}/fabricantes`, fabricante);
   }
   crearEscoba(escoba: Escoba, fabricanteId: number): Observable<Escoba> {
     // URL: /api/escobas/fabricante/{fabricanteId}
-    return this.http.post<Escoba>(`https://semihumanized-stoopingly-emerald.ngrok-free.dev/api/escobas/fabricante/${fabricanteId}`, escoba);
+    return this.http.post<Escoba>(`${API_URL}/escobas/fabricante/${fabricanteId}`, escoba);
   }
   // NUEVO: Método para ACTUALIZAR una Escoba (PUT /api/escobas/{id})
   actualizarEscoba(id: number, escoba: Escoba): Observable<Escoba> {
     // Nota: enviamos el objeto Escoba completo, incluyendo el ID en la URL
-    return this.http.put<Escoba>(`https://semihumanized-stoopingly-emerald.ngrok-free.dev/api/escobas/${id}`, escoba);
+    return this.http.put<Escoba>(`${API_URL}/escobas/${id}`, escoba);
   }
   actualizarFabricante(id: number, fabricante: Fabricante): Observable<Fabricante> {
     // Nota: enviamos el objeto Fabricante completo, incluyendo el ID en la URL
-    return this.http.put<Fabricante>(`https://semihumanized-stoopingly-emerald.ngrok-free.dev/api/fabricantes/${id}`, fabricante);
+    return this.http.put<Fabricante>(`${API_URL}/fabricantes/${id}`, fabricante);
   }
 }
