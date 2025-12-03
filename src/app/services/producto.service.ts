67a3e51a49eedda04d831a2668e0c7ffcef399d3
DanielaAlejandraMamaniSilva <danielaalejandramamanisilva@gmail.com>
2025-12-03 00:36:04 -0400

    deploy backend fix

--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 // La URL Base de tu Backend
-const API_URL = 'backendsis4142025-production.up.railway.app/api'; 
+const API_URL = 'https://backendsis4142025-production.up.railway.app/api'; 
 
 // --- Interfaces del Backend ---
 export interface Escoba { 
